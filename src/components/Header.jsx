40ab92573eb23a0bd81aa2fea0e31780d1c543d4
Vasuvj93 <kvasudevan499@gmail.com>
2025-06-22 23:11:37 +0530

    Update Header.jsx
    Spll mistake

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header(props){
     return(
     <div>
          <h1 className="text-3xl font-medium ">Hello {props.name}</h1>
-        <p>I help you manage yur activities</p>
+        <p>I help you manage your activities</p>
 
     </div>)
 }
